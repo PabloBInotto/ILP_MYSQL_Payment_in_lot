070576fbc303aa83f054ce4923da6cb7b9cc6480
Pablo_Binotto <binhotto1978@gmail.com>
2019-05-11 23:55:01 -0300

    Create querypay.js

---
+++ querypay.js
@@ -0,0 +1,19 @@
+const pay = require('./ilp')
+const con = require('./conn');
+function querypay(){
+
+  con.query("SELECT * FROM `pay_list` where `status` = 1", function (err, result, fields) {
+    if (err) throw err;
+    if (result.length > 0) {
+        for (var i = 0; i < result.length; i++) {
+            // use '$spsp.ilp-test.com' if you're on the testnet
+               pay( result[i].receiver,
+                result[i].sourceAmount)
+            console.log('Receiver: ', result[i].receiver);
+            console.log('sourceAmount: ', result[i].sourceAmount);                
+        }
+    } else {console.log("Nobody to pay")}
+    });    
+}
+      
+    module.exports = querypay;
