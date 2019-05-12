ca8a8865d79c020dbfb1e4c34c00d61799c43230
Pablo_Binotto <binhotto1978@gmail.com>
2019-05-11 23:53:11 -0300

    Update ilp.js
    use the mysql on extarnal file

--- ilp.js
+++ ilp.js
@@ -4,21 +4,12 @@ function pay(){
     const con = require('./conn');
     async function run () {
       console.log('make payment in lot every 5 secounds')
-      con.query("SELECT * FROM `pay_list` where `status` = 1", function (err, result, fields) {
-        if (err) throw err;
-        if (result.length > 0) {
-            for (var i = 0; i < result.length; i++) {
-                // use '$spsp.ilp-test.com' if you're on the testnet
-                SPSP.pay(plugin, {
-                    receiver: result[i].receiver,
-                    sourceAmount: result[i].sourceAmount
+               // use '$spsp.ilp-test.com' if you're on the testnet
+               await SPSP.pay(plugin, {
+                   
                 })
-                console.log('Receiver: ', result[i].receiver);
-                console.log('sourceAmount: ', result[i].sourceAmount);                
-            }
-        } else {console.log("Nobody to pay")}
-        });    
-      console.log('1 minutes wages paid to your paymentpointer!')
+               
+      console.log('5 secounds wages paid to your paymentpointer!')
       }
     run().catch(e => console.error(e))
     }
