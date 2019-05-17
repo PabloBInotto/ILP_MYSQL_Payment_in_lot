d30fa15f9d6f04e9b1673b9a08d8ed5f122d0c96
Pablo_Binotto <binhotto1978@gmail.com>
2019-05-16 22:09:15 -0300

    include deductions
    include deductions tax and pension to each worker

--- querypay.js
+++ querypay.js
@@ -2,18 +2,18 @@ const pay = require('./ilp')
 const con = require('./conn');
 function querypay(){
 
-  con.query("SELECT * FROM `pay_list` where `status` = 1", function (err, result, fields) {
+  con.query("SELECT * FROM `pay_and_deductions`", function (err, result, fields) {
     if (err) throw err;
     if (result.length > 0) {
         for (var i = 0; i < result.length; i++) {
-            // use '$spsp.ilp-test.com' if you're on the testnet
-               pay( result[i].receiver,
-                result[i].sourceAmount)
-            console.log('Receiver: ', result[i].receiver);
-            console.log('sourceAmount: ', result[i].sourceAmount);                
+
+               pay(result[i].receiver, result[i].sourceAmount) // pay receiver
+               pay(result[i].receiver_tax, result[i].sourceAmout_tax) // pay tax
+               pay(result[i].receiver_pension, result[i].souceAmount_pension) // pay pencion
+                
         }
     } else {console.log("Nobody to pay")}
-    });    
+    });     
 }
       
     module.exports = querypay;
